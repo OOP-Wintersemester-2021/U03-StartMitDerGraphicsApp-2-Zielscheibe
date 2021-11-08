56b270a8837545bdd5eca4516ad07a2c619a73f1
Jannik Wiese <69634980+ragor114@users.noreply.github.com>
2021-11-08 12:42:32 +0100

    Added Comments

--- src/Target/Target.java
+++ src/Target/Target.java
@@ -13,6 +13,7 @@ public class Target extends GraphicsApp {
     private static final int CANVAS_WIDTH = 800;
     private static final Color BACKGROUND_COLOR = Colors.WHITE;
 
+    // Das Zentrum des Kreises wird auf die Mitte der Zeichenfläche festgelegt
     private static final int HORIZONTAL_CENTER = CANVAS_WIDTH / 2;
     private static final int VERTICAL_CENTER = CANVAS_HEIGHT / 2;
 
@@ -36,7 +37,6 @@ public class Target extends GraphicsApp {
      * Die draw-Methode wird so lange wiederholt aufgerufen, bis das Programm
      * beendet wird.
      */
-
     @Override
     public void draw() {
         drawBackground(BACKGROUND_COLOR);
@@ -48,6 +48,21 @@ public class Target extends GraphicsApp {
         drawBackground(BACKGROUND_COLOR);
     }
 
+    /*
+        Die Kreisfarbe soll sich abwechseln, daher ermittelt man, ob die aktuelle Nummer des Kreises
+        gerade oder ungerade ist und legt dementsprechend die Farbe fest. Der Test auf gerade oder ungerade
+        erfolgt durch den Modulo-Operator (%), dieser liefert den Rest einer Division. Eine Zahl % 2 liefert
+        daher entweder 1 oder 0. Ist das Ergebnis 1 ist die Zahl ungerade, da sie nicht durch 2 ohne
+        Rest teilbar ist, ist das Ergebnis 0 ist die Zahl gerade, da sie ohne Rest durch 2 teilbar
+        ist.
+        Der Radius jedes Rings ist der initiale Radius von 10 + die Nummer des Rings mal den initialen
+        Radius. Dadurch hat der erste Ring den initialen Radius, der zweite 2 * den initialen Radius, der
+        dritte 3 * den initialen Radius usw.
+        Die for-Schleife zählt von der Zahl der zu erzeugenden Ringe bis 0 runter. Denn später gezeichnete
+        Objekte werden in der GraphicsApp als oberstes angezeigt. Zeichnet man daher den größten Ring zu letzt
+        wäre dieser das oberste Element und die anderen nicht mehr sichtbar. Der größte Ring muss daher zuerst#
+        gezeichnet werden und der kleinste Ring als letztes.
+    */
     private void drawDartTarget() {
         Color ringColor;
         int radius;
